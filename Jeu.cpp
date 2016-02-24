a6465c3663af75ceac0f2682cee7acdbd761718c
Anthony <anthony.carmona@etu.umontpellier.fr>
2016-02-24 14:02:58 +0100

    Ajout des modifications sur la classe Jeu

--- Jeu.cpp
+++ Jeu.cpp
@@ -1,4 +1,44 @@
-//
-// Created by stardisblue on 12/02/16.
-//
+#include "Jeu.h"
+
+Jeu::Jeu(int sizeJeu){
+
+	//Initialize Variables
+	this->size = sizeJeu;
+	this->jeu = new Piece[this->size];
+	this->TabC = new Piece[4];
+	this->TabB = new Piece[(this->size-2)*4];
+
+	int size_of_tab_I = pow(this->size-2,2);
+
+	this->TabI = new Piece[sizeOfTabI];
+	this->NbPiecesPresentes = 0;
+
+}
+
+int Jeu::addPiece(Piece p){
+
+	if(this->nb_pieces_presentes < pow(this->size,2)){
+		this->jeu[this->nb_pieces_presentes] = p;
+		this->nb_pieces_presentes++;
+	}
+
+}
+
+// Getters 
+int Jeu::getSize(){
+	return this->size;
+}
+Piece* Jeu::getJeu(){
+	return this->jeu;
+}
+Piece* Jeu::getTabC(){
+	return this->TabC;
+}
+Piece* Jeu::getTabB(){
+	return this->TabB;
+}
+Piece* Jeu::getTabI(){
+	return this->TabI;
+}
+
 
--- Jeu.h
+++ Jeu.h
@@ -1,23 +1,37 @@
-//
-// Created by stardisblue on 12/02/16.
-//
-
 #ifndef JEU_H
 #define JEU_H
 
+#include <math.h>
+#include <iostream>
 
 #include "Piece.h"
 
-class Jeu
-{
-private:
-    Piece tabC[];
-    Piece tabB[];
-    Piece tabI[];
 
-    int size;
-    Piece pieces[];
+class Jeu {
+	
+	public:
+
+	Jeu(int size); //constructeur
+	int addPiece(Piece p); //Ajout d'une piece dans le jeu
+
+	//Getters 
+	int getSize();
+	Piece* getJeu();
+	Piece* getTabC();
+	Piece* getTabB();
+	Piece* getTabI();
+
+
+	private:
+
+	int size;
+	Piece* jeu;
+	Piece* TabC;
+	Piece* TabB;
+	Piece* TabI;
+
+	int nb_pieces_presentes;
+
 };
 
-
-#endif //JEU_H
+#endif
